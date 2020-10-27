26f239379a0ddacbb3084d59d6ed7159f7b5cdab
Rafael-93 <43275288+Rafael-93@users.noreply.github.com>
2020-10-27 15:57:45 -0300

    A `public funcion home` do controller Login foi excluida pq nao estava sendo utilizada

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,11 +8,7 @@ class Login extends CI_Controller{
         $this->load->view('login-view'); // Carrega a view de login
     }
 
-    public function home(){
-
-      $this->load->view('home-view');
-    }
-
+    
     public function entradaPresos(){
       $this->load->view('entrada-presos-view');
     }
